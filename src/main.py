c9bf1efad35a2bc5977235ec7a418c693c3f1cff
Sagar <sagardesaee@gmail.com>
2023-11-16 09:48:02 +0530

    chnaes related to pre and post release of packages

--- src/main.py
+++ src/main.py
@@ -1,9 +1,1 @@
-import sys
-
-print(sys.path)
-
-from nest_folder.my_nest import nested_var
-
-print(nested_var)
-
-main_var = "main variable"
+print("hello")
--- src/nest_folder/my_nest.py
+++ src/nest_folder/my_nest.py
@@ -1,5 +1,3 @@
-from ..main import main_var
-
 nested_var = "nested variable"
 
-print(main_var)
+print(nested_var)
